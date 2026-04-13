8d4ec8cc84ce9c0018d8da2313c475598df07a89
ruya841 <rtarq99@gmail.com>
2026-04-13 18:19:40 +0300

    first commit

---
+++ src/main/java/com/mycompany/yahtzee_networkproject/Yahtzee_NetworkProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.yahtzee_networkproject;
+
+/**
+ *
+ * @author USER
+ */
+public class Yahtzee_NetworkProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
